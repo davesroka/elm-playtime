6001c3646591a971e77aef3b0e7bbba1ceb7d35a
Dave Sroka <dave@davesroka.net>
2017-03-04 11:52:40 -0500

    Add webpack and some dependencies
    Enables webpack dev server

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+var path = require('path');
+
+module.exports = {
+  entry: {
+    app: ['./src/index.js']
+  },
+  output: {
+    path: path.resolve(__dirname + '/dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [{
+        test: /\.(css|scss)$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]'
+          }
+        }]
+      },
+      {
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        use: [{
+          loader: 'elm-webpack-loader',
+          options: {
+            verbose: true,
+            warn: true
+          }
+        }]
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            mimetype: 'application/font-woff'
+          }
+        }]
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: ['file-loader']
+      }
+    ],
+    noParse: /\.elm$/
+  },
+  devServer: {
+    inline: true,
+    stats: {
+      colors: true
+    }
+  }
+};
