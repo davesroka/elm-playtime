bbc895c744753d971825130d2485014f51cbb7c9
Dave Sroka <dave@davesroka.net>
2017-03-07 07:41:18 -0500

    Update widget

--- src/Pages/Widgets/State.elm
+++ src/Pages/Widgets/State.elm
@@ -1,9 +1,26 @@
 module Pages.Widgets.State exposing (..)
 
 
+type alias Id =
+    String
+
+
+type alias Name =
+    String
+
+
+type alias HexCode =
+    String
+
+
 type alias Widget =
-    { name : String
-    , hexCode : String
+    { id : Id
+    , name : Name
+    , hexCode : HexCode
     }
 
 
+type Msg
+    = ShowWidgets
+    | ShowWidget Id
+    | AddWidget Name HexCode
